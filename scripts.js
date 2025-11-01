bf2ef20aaec0d3395547081b8f275e7cd4123f8b
Marissa O'Meally <marissaomeally21@gmail.com>
2025-11-01 14:42:13 -0500

    Updated files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function(){
+    const form = document.querySelector("form");
+    const Message = document.querySelector(".message");
+
+
+    // Add an event listener for form submission
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        
+        // Sanitize user input
+        const emailData = document.getElementById('email').value;
+        const email = emailData.replace(/</g, "&lt;").replace(/>/g, "&gt;").trim();
+        
+
+        // Check if email has entered
+        if(email === ""){
+            Message.textContent = "Please enter a valid email address.";
+            Message.style.color = "red";
+        }
+        else{
+            Message.innerHTML= `<p>Thank you! Your email address <strong>${email}</strong> has been added to our mailing list!</p>`;
+            Message.style.color = "blue"; 
+        }
+    });
+});
